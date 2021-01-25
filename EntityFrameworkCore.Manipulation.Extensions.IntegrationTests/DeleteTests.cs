66cd35320e6e88a13bc9e13b8c0ec1d8766b5d19
Sebastian Blomberg <seblombe@microsoft.com>
2021-01-25 17:50:28 +0000

    Adds support for bulk delete with output

---
+++ EntityFrameworkCore.Manipulation.Extensions.IntegrationTests/DeleteTests.cs
@@ -0,0 +1,118 @@
+﻿using EntityFrameworkCore.Manipulation.Extensions;
+using EntityFrameworkCore.Manipulation.Extensions.IntegrationTests.Helpers;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Manipulation.Extensions.UnitTests
+{
+    [TestClass]
+    public class DeleteTests
+    {
+        [TestMethod]
+        public async Task DeleteAsync_ShouldReturnEmptyCollection_WhenThereAreNoEntities()
+        {
+            using TestDbContext context = await this.GetDbContext(seedData: null); // Note: no seed data => no entities exist
+
+			// Invoke the method and check that the result is empty
+			IReadOnlyCollection<TestEntity> result = await context.DeleteAsync(context.TestEntities);
+
+            result.Should().BeEmpty();
+        }
+
+		[TestMethod]
+		public async Task DeleteAsync_ShouldReturnEmptyCollection_WhenThereAreNoMatchingEntities()
+		{
+			var existingEntities = new[]
+			{
+				new TestEntity { Id = "Should not be touched 1", IntTestValue = 561645, BoolTestValue = false, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 54123 },
+				new TestEntity { Id = "Should not be touched 2", IntTestValue = 111, BoolTestValue = true, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 65465132165 },
+			};
+
+			using TestDbContext context = await this.GetDbContext(existingEntities);
+
+			// Invoke the method and check that the result is empty
+			IReadOnlyCollection<TestEntity> result = await context.DeleteAsync(context.TestEntities.Where(entity => entity.Id == "Does not exist"));
+
+			result.Should().BeEmpty();
+
+			// Check that the DB hasn't been modified
+			context.TestEntities.ToList().Should().BeEquivalentTo(existingEntities);
+		}
+
+		[TestMethod]
+		public async Task DeleteAsync_ShouldReturnEntireCollection_WhenDeleteTargetIsAllEntities()
+		{
+			var existingEntities = new[]
+			{
+				new TestEntity { Id = "Should be delete 1", IntTestValue = 561645, BoolTestValue = false, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 54123 },
+				new TestEntity { Id = "Should be delete 2", IntTestValue = 111, BoolTestValue = true, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 65465132165 },
+				new TestEntity { Id = "Should be delete 3", IntTestValue = 891564, BoolTestValue = true, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 894156156 },
+			};
+
+			using TestDbContext context = await this.GetDbContext(existingEntities);
+
+			// Invoke the method and check that the result is all entities
+			IReadOnlyCollection<TestEntity> result = await context.DeleteAsync(context.TestEntities);
+
+			result.Should().BeEquivalentTo(existingEntities);
+
+			// Check that the DB is empty
+			context.TestEntities.Should().BeEmpty();
+		}
+
+		[TestMethod]
+		public async Task DeleteAsync_ShouldReturnMatchingDeleteCollection_WhenDeleteTargetIsASubsetOfCollection()
+		{
+			var existingEntities = new[]
+			{
+				new TestEntity { Id = "Should be delete 1", IntTestValue = -1321, BoolTestValue = false, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 54123 },
+				new TestEntity { Id = "Should not be delete 2", IntTestValue = 111, BoolTestValue = true, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 65465132165 },
+				new TestEntity { Id = "Should be delete 3", IntTestValue = -516, BoolTestValue = true, DateTimeTestValue = DateTime.UtcNow, LongTestValue = 894156156 },
+			};
+
+			using TestDbContext context = await this.GetDbContext(existingEntities);
+
+			// Invoke the method and check that the result is the subset of entities with a negative IntTestValue 
+			IReadOnlyCollection<TestEntity> result = await context.DeleteAsync(context.TestEntities.Where(entity => entity.IntTestValue < 0));
+
+			result.Should().BeEquivalentTo(existingEntities.Where(entity => entity.IntTestValue < 0));
+
+			// Check that the DB is empty
+			context.TestEntities.Should().BeEquivalentTo(existingEntities.Where(entity => entity.IntTestValue >= 0));
+		}
+
+		private async Task<TestDbContext> GetDbContext(IEnumerable<TestEntity> seedData = null)
+        {
+            // var sqlConnection = new SqliteConnection("Data Source=F:\\temp\\temp.db;");
+            // var sqlConnection = new SqliteConnection("Data Source=:memory:;");
+            var sqlConnection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True;");
+            await sqlConnection.OpenAsync();
+
+            var optionsBuilder = new DbContextOptionsBuilder();
+
+            optionsBuilder.UseSqlServer(sqlConnection).EnableSensitiveDataLogging(true);
+			// optionsBuilder.UseSqlite(sqlConnection).EnableSensitiveDataLogging(true);
+
+			var command = sqlConnection.CreateCommand();
+			command.CommandText = "DELETE FROM TestEntities";
+			command.ExecuteNonQuery();
+
+			if (seedData != null)
+            {
+                using var seedContext = new TestDbContext(optionsBuilder.Options);
+				seedContext.Database.EnsureCreated();
+				seedContext.AddRange(seedData);
+                await seedContext.SaveChangesAsync();
+            }
+
+            return new TestDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ EntityFrameworkCore.Manipulation.Extensions/DeleteExtensions.cs
@@ -0,0 +1,73 @@
+﻿using EntityFrameworkCore.Manipulation.Extensions.Internal;
+using EntityFrameworkCore.Manipulation.Extensions.Internal.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Manipulation.Extensions
+{
+	public static class DeleteExtensions
+	{
+		/// <summary>
+		/// Deletes all entities in a target collection (<paramref name="deleteTarget"/>),
+		/// and returns the deleted entities.
+		/// </summary>
+		/// <typeparam name="TEntity">The type of entity.</typeparam>
+		/// <param name="dbContext">The <see cref="DbContext"/> to operate on.</param>
+		/// <param name="deleteTarget">The target of entities to delete.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <returns>The collection of entities which were deleted.</returns>
+		public static Task<IReadOnlyCollection<TEntity>> DeleteAsync<TEntity>(this DbContext dbContext, IQueryable<TEntity> deleteTarget, CancellationToken cancellationToken = default)
+			where TEntity : class
+			=> DeleteInternalAsync(
+				dbContext ?? throw new ArgumentNullException(nameof(dbContext)),
+				deleteTarget ?? throw new ArgumentNullException(nameof(deleteTarget)),
+				cancellationToken);
+
+		private static async Task<IReadOnlyCollection<TEntity>> DeleteInternalAsync<TEntity>(this DbContext dbContext, IQueryable<TEntity> deleteTarget, CancellationToken cancellationToken = default)
+			where TEntity : class
+		{
+			(string targetCommand, IReadOnlyCollection<SqlParameter> targetCommandParameters) = deleteTarget.ToSqlCommand();
+
+			var stringBuilder = new StringBuilder(300);
+
+			if (dbContext.Database.IsSqlite())
+			{
+				const string TempDeleteTableName = "EntityFrameworkManipulationDelete";
+				IEntityType entityType = dbContext.Model.FindEntityType(typeof(TEntity));
+
+				string tableName = entityType.GetTableName();
+				IKey primaryKey = entityType.FindPrimaryKey();
+
+				stringBuilder.AppendLine("BEGIN TRANSACTION;")
+							 .Append("DROP TABLE IF EXISTS ").Append(TempDeleteTableName).AppendLine(";")
+							 .Append("CREATE TEMP TABLE ").Append(TempDeleteTableName).Append(" AS ").Append(targetCommand).AppendLine(";");
+
+				stringBuilder
+					.Append("DELETE FROM ").AppendLine(tableName)
+					.Append("WHERE EXISTS (SELECT 1 FROM ").Append(TempDeleteTableName).Append(" WHERE ").AppendJoinCondition(primaryKey, tableName, TempDeleteTableName).AppendLine(");");
+
+				stringBuilder.Append("SELECT * FROM ").Append(TempDeleteTableName).AppendLine(";");
+				stringBuilder.AppendLine("COMMIT;");
+			}
+			else
+			{
+				stringBuilder
+					.AppendLine("WITH DeleteTarget AS (").Append(targetCommand).AppendLine(")")
+					.AppendLine("DELETE FROM DeleteTarget ")
+					.AppendLine("OUTPUT DELETED.* ");
+			}
+
+			return await dbContext.Set<TEntity>().FromSqlRaw(stringBuilder.ToString())
+												.AsNoTracking()
+												.ToListAsync(cancellationToken)
+												.ConfigureAwait(false);
+		}
+	}
+}
